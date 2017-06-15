4463515a00e7f02bc30761335604611a3f2df010
jihyoen lee <dlwlgus9125@naver.com>
2017-06-15 16:22:39 +0900

    아이템DB테스트용

--- TeamPortPolio/TeamPortPolio/cItemDB.cpp
+++ TeamPortPolio/TeamPortPolio/cItemDB.cpp
@@ -10,3 +10,26 @@ cItemDB::cItemDB()
 cItemDB::~cItemDB()
 {
 }
+
+void cItemDB::Setup()
+{
+	Add(new ST_ITEM(I_L_MELEE, I_M_SWORD, I_S_SHORTSWORD,"¼ô ¼Òµå", "°¡Àå ÈçÇÑ ÂªÀº °ËÀÌ´Ù.", 50, ""));
+}
+
+void cItemDB::Add(ST_ITEM * pData)
+{
+	if (m_mapObject.find(pData->eSmallID) == m_mapObject.end())
+	{
+		m_mapObject[pData->eSmallID] = pData;
+	}
+}
+
+void cItemDB::Destroy()
+{
+	for each(auto it in m_mapObject)
+	{
+		SAFE_DELETE(it.second);
+	}
+
+	delete this;
+}
